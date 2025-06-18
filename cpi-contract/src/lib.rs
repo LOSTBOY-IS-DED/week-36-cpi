166882d1e21142f4883216709c91f329888e7f23
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-18 09:06:52 +0530

    pushed completely

--- cpi-contract/src/lib.rs
+++ cpi-contract/src/lib.rs
@@ -2,25 +2,21 @@ use solana_program::{
     account_info::{AccountInfo, next_account_info},
     entrypoint,
     entrypoint::ProgramResult,
-    instruction::{AccountMeta, Instruction}, // ✅ Added
-    program::invoke,                         // ✅ Added
+    instruction::{AccountMeta, Instruction},
+    program::invoke,
     pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    publicKey: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
     let mut iter = accounts.iter();
     let data_account = next_account_info(&mut iter)?;
     let double_contract_address = next_account_info(&mut iter)?;
-
-    // get accounts and invoke
-    // invoke takes instruction and account_infos --> account_infos are bunch of account the next contract excepts
-    // instruction is similar as previous instruction
 
     let instruction = Instruction {
         program_id: *double_contract_address.key,
@@ -32,6 +28,6 @@ pub fn process_instruction(
         data: vec![],
     };
 
-    let res = invoke(&instruction, &[data_account.clone()])?;
-    Ok(());
+    invoke(&instruction, &[data_account.clone()])?;
+    Ok(())
 }
